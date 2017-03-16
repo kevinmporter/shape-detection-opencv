0bbac95e834f64baeba4ca2764856a56bccd67be
Kevin Porter <kevin@kevinporter.me>
2017-03-16 09:14:08 -0400

    Hopefully didn't screw anything up

--- UAS_Detection/Detection.py
+++ UAS_Detection/Detection.py
@@ -1,6 +1,14 @@
+from subprocess import call
+from threading import Thread
 import numpy as np
 import cv2
+import datetime
 
+
+def pull_key_frames(path, flag):
+    flag = True
+    call('ffmpeg -i ' + path + ' -vf select="eq(pict_type\,PICT_TYPE_I)" -vsync 2 -f image2 tmp/thumbnails-%02d.jpeg')
+    flag = False
 
 def inside(r, q):
     rx, ry, rw, rh = r
@@ -20,13 +28,19 @@ def draw_detections(img, rects, thickness = 1):
 if __name__ == '__main__':
 
     hog = cv2.HOGDescriptor()
-    hog.setSVMDetector( cv2.HOGDescriptor_getDefaultPeopleDetector() )
-    cap=cv2.VideoCapture('ScreenCaptureProject4.avi')
-    while True:
-        _,frame=cap.read()
-        found,w=hog.detectMultiScale(frame, winStride=(8,8), padding=(32,32), scale=1.05)
-        draw_detections(frame,found)
-        cv2.imshow('feed',frame)
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    cap = cv2.VideoCapture('/Users/porterkm/Projects/shape-detection-opencv/UAS_Detection/dumb.mp4')
+    '''
+    ffmpeg -i dumb.mp4 -vf select="eq(pict_type\,PICT_TYPE_I)" -vsync 2 -f image2 tmp/thumbnails-%02d.jpeg
+    '''
+    flag = True
+    pull_key_frames('dumb.mp4', flag)
+    while flag:
+        _, frame = cap.read()
+        print datetime.datetime.now()
+        found, w = hog.detectMultiScale(frame, winStride=(8, 8), padding=(32, 32), scale=1.05)
+        draw_detections(frame, found)
+        cv2.imshow('feed', frame)
         ch = 0xFF & cv2.waitKey(1)
         if ch == 27:
             break
