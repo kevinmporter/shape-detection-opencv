36f0908c9bc47f70b190f4c6949d64b68f00c31a
Kevin Porter <kevin@kevinporter.me>
2017-03-21 21:49:51 -0400

    Algorithm for pulling out keyframes works

--- UAS_Detection/Detection.py
+++ UAS_Detection/Detection.py
@@ -3,12 +3,12 @@ from threading import Thread
 import numpy as np
 import cv2
 import datetime
+import os
 
 
-def pull_key_frames(path, flag):
-    flag = True
-    call('ffmpeg -i ' + path + ' -vf select="eq(pict_type\,PICT_TYPE_I)" -vsync 2 -f image2 tmp/thumbnails-%02d.jpeg')
-    flag = False
+def pull_key_frames(path):
+    os.system('/usr/local/bin/ffmpeg -i ' + path + ' -vf select="eq(pict_type\,PICT_TYPE_I)" -vsync 2 -f image2 tmp/%d.jpeg')
+
 
 def inside(r, q):
     rx, ry, rw, rh = r
@@ -16,7 +16,7 @@ def inside(r, q):
     return rx > qx and ry > qy and rx + rw < qx + qw and ry + rh < qy + qh
 
 
-def draw_detections(img, rects, thickness = 1):
+def draw_detections(img, rects, thickness=1):
     for x, y, w, h in rects:
         # the HOG detector returns slightly larger rectangles than the real
         #  objects.
@@ -29,19 +29,20 @@ if __name__ == '__main__':
 
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-    cap = cv2.VideoCapture('/Users/porterkm/Projects/shape-detection-opencv/UAS_Detection/dumb.mp4')
-    '''
-    ffmpeg -i dumb.mp4 -vf select="eq(pict_type\,PICT_TYPE_I)" -vsync 2 -f image2 tmp/thumbnails-%02d.jpeg
-    '''
-    flag = True
-    pull_key_frames('dumb.mp4', flag)
-    while flag:
-        _, frame = cap.read()
-        print datetime.datetime.now()
-        found, w = hog.detectMultiScale(frame, winStride=(8, 8), padding=(32, 32), scale=1.05)
-        draw_detections(frame, found)
-        cv2.imshow('feed', frame)
-        ch = 0xFF & cv2.waitKey(1)
-        if ch == 27:
-            break
+    thread = Thread(target=pull_key_frames, args=('dumb.mp4', ))
+    thread.start()
+    #pull_key_frames('dumb.mp4')
+    num = 1
+    while True:
+        path = os.path.join('tmp', str(num) + '.jpeg')
+        if os.path.isfile(path):
+            num += 1
+            frame = cv2.imread(path)
+            print datetime.datetime.now()
+            found, w = hog.detectMultiScale(frame, winStride=(8, 8), padding=(32, 32), scale=1.05)
+            draw_detections(frame, found)
+            cv2.imshow('feed', frame)
+            ch = 0xFF & cv2.waitKey(1)
+            if ch == 27:
+                break
     cv2.destroyAllWindows()
